0c5cc881f5d862b58cf59a9296cad4ab6008029a
Seo Songhyeon <songhyeon4643@gmail.com>
2022-11-01 23:03:41 +0900

    thymleaf

--- src/main/java/togethers/togethers/TogetherController.java
+++ src/main/java/togethers/togethers/TogetherController.java
@@ -1,5 +1,6 @@
 package togethers.togethers;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j
 public class TogetherController {
+
     @RequestMapping("/")
-    public String header(Model model){
-        log.info("home controller");
-        return "css/header";
+    public String main(Model model) {
+        model.addAttribute("data","hello!!!!");
+        return "html/footer";
     }
+
+    @GetMapping("/home")
+    public String home() {
+        return "html/header";
+    }
+
 }
