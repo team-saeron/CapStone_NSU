30172ffe8640852ffd8e0c57d6eb35992a83f7ab
Seo Songhyeon <songhyeon4643@gmail.com>
2022-11-03 15:35:13 +0900

    fragments

--- src/main/java/togethers/togethers/HomeController.java
+++ src/main/java/togethers/togethers/HomeController.java
@@ -5,13 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String main(Model model) {
-        //String strYear = (String)request.getAttribute("strYear");
         model.addAttribute("date","2022");
         return "home";
     }
