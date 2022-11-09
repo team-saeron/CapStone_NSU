09f89d6a2729590bfde11ecba94f900b3b7535e2
hokyun <wjdghrbs2468@naver.com>
2022-11-09 14:36:26 +0900

    hokyun test commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# CapStone_NSU
+
+## 👨‍💻팀원
+<p>
+<a href="https://github.com/shyeon4643">
+  <img src="https://avatars.githubusercontent.com/u/62410059?v=4" width="100">
+</a>
+<a href="https://github.com/Kor-YJ">
+  <img src="https://avatars.githubusercontent.com/u/101186255?s=60&v=4" width="100">
+</a>
+<a href="https://github.com/bhcvanvanmumani">
+  <img src="https://avatars.githubusercontent.com/u/101186255?s=60&v=4" width="100">
+</a>
+<a href="https://github.com/hokyun-tazo">
+  <img src="https://avatars.githubusercontent.com/u/101186255?s=60&v=4" width="100">
+</a>
+</p>
+
---
+++ src/main/java/togethers/togethers/HomeController.java
@@ -0,0 +1,29 @@
+package togethers.togethers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String main(Model model) {
+        model.addAttribute("date","2022");
+        return "home";
+    }
+
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "member/join";
+    }
+}
+
