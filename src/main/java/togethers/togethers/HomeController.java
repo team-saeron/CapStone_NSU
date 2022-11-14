c95f8f60b505a657f3eea5a2286728bc38dad50e
Seo Songhyeon <songhyeon4643@gmail.com>
2022-11-14 12:28:32 +0900

    회원가입 페이지 레이아웃

--- src/main/java/togethers/togethers/HomeController.java
+++ src/main/java/togethers/togethers/HomeController.java
@@ -21,14 +21,12 @@ public class HomeController {
         return "home";
     }
 
-<<<<<<< HEAD
-=======
+
     @GetMapping("/agree")
     public String agree(){
         return "member/agree";
     }
 
->>>>>>> aff2bb1da86941373135a1cdbb44706dc94ba6bc
     @GetMapping("/join")
     public String join(){
         return "member/join";
