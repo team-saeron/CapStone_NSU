88eacb1af55069d6bf6cf3159c1e1ba6a4f5eebf
Seo Songhyeon <songhyeon4643@gmail.com>
2022-11-19 00:37:56 +0900

    controller 변경

--- src/main/java/togethers/togethers/member/MemberController.java
+++ src/main/java/togethers/togethers/member/MemberController.java
@@ -2,12 +2,11 @@ package togethers.togethers.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequiredArgsConstructor
-public class memberController {
+public class MemberController {
     @GetMapping("/member/mypage")
     public String mypage(){ return "member/mypage";}
     @GetMapping("/member/introduction")
