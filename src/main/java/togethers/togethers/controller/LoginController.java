9dc44a604ff1864c455e63f0f157cdd3de7da69e
haneul <yoon0011282822@gmail.com>
2023-01-17 13:45:27 +0900

    마이페이지,회원가입 변경

--- src/main/java/togethers/togethers/controller/LoginController.java
+++ src/main/java/togethers/togethers/controller/LoginController.java
@@ -5,10 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import togethers.togethers.item.Member;
 
 
 import javax.servlet.http.Cookie;
