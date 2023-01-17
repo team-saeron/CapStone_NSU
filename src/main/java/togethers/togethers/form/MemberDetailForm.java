139cec7271bf7b0f7b825d65cad3c9af2d4cbb42
Kor-YJ <dongweoof@gmail.com>
2023-01-17 13:58:05 +0900

    게시글 db 서

---
+++ src/main/java/togethers/togethers/form/MemberDetailForm.java
@@ -0,0 +1,25 @@
+package togethers.togethers.form;
+
+import lombok.Data;
+import togethers.togethers.entity.*;
+
+@Data
+public class MemberDetailForm {
+    Long memberDetail_Id;
+    int mbti;
+    String selfIntro;
+    String wish_roommate;
+    Member member;
+
+    String mouthly_fee;
+    String lease_fee;
+    int area_id;
+
+    int smoking;
+    int pet;
+    int roomType;
+    int lifeCycle;
+    int sex;
+
+
+}
