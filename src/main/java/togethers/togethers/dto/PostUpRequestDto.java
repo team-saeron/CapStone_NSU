226e44e363394ddf1f9b568aad209351a00d6947
hokyun <wjdghrbs2468@naver.com>
2023-02-03 15:41:38 +0900

    게시물 등록과 유저 연관관계 세팅

---
+++ src/main/java/togethers/togethers/dto/PostUpRequestDto.java
@@ -0,0 +1,18 @@
+package togethers.togethers.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Builder
+@Getter
+@Setter
+public class PostUpRequestDto {
+    private String title;
+    private Boolean getType;
+    private Boolean roomType;
+    private String mouthly;
+    private String lease;
+    private String text;
+}
