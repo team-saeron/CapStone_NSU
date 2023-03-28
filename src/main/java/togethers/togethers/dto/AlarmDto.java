95dee21e628dadcba7e6c746c23d42e16c58f158
Seo Songhyeon <songhyeon4643@gmail.com>
2023-03-28 14:29:39 +0900

    Revert "알람 구현 삭제"
    This reverts commit 247ddaf8fb949a2e6e459de6e1349df1a586f25d.

---
+++ src/main/java/togethers/togethers/dto/AlarmDto.java
@@ -0,0 +1,16 @@
+package togethers.togethers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class AlarmDto {
+    String title;
+    String id;
+
+}
---
+++ src/main/java/togethers/togethers/service/AlarmService.java
@@ -0,0 +1,9 @@
+package togethers.togethers.service;
+
+import togethers.togethers.dto.AlarmDto;
+
+public interface AlarmService {
+    public void alarmByMessage(AlarmDto alarmDto);
+
+
+}
