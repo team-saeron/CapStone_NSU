d499bb4b4e24a8e316fd45b87d9cf700518d34ff
Seo Songhyeon <songhyeon4643@gmail.com>
2023-05-24 00:23:53 +0900

    지도 검색

--- src/main/java/togethers/togethers/controller/HomeController.java
+++ src/main/java/togethers/togethers/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import togethers.togethers.Enum.AreaEnum;
 import togethers.togethers.dto.post.PostSearchDto;
 import togethers.togethers.dto.post.RecentlyPostDto;
